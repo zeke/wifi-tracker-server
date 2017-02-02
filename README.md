0eb8a00eea7f53ec5ce099fc7bf7608a6180107b
Zeke Sikelianos <zeke@sikelianos.com>
2017-02-01 21:26:05 -0800

    all the things

---
+++ README.md
@@ -0,0 +1,19 @@
+# Electron API Demos Server
+
+Squirrel update server for the Electron API Demos app.
+
+This app is hosted at `https://electron-api-demos.githubapp.com` via Heroku
+under the [github-electron-api-demos](https://dashboard.heroku.com/apps/github-electron-api-demos)
+app name.
+
+Pushes to master are automatically deployed.
+
+## Downloading Releases
+
+The following convenience URLs redirect to the latest release for a given platform:
+
+- [updates/electron-api-demos-linux.zip](https://electron-api-demos.githubapp.com/updates/electron-api-demos-linux.zip)
+- [updates/electron-api-demos-mac.zip](https://electron-api-demos.githubapp.com/updates/electron-api-demos-mac.zip)
+- [updates/electron-api-demos-windows.zip](https://electron-api-demos.githubapp.com/updates/electron-api-demos-windows.zip)
+- [updates/ElectronAPIDemos-0.4.0-full.nupkg](https://electron-api-demos.githubapp.com/updates/ElectronAPIDemos-0.4.0-full.nupkg)
+- [updates/ElectronAPIDemosSetup.exe](https://electron-api-demos.githubapp.com/updates/ElectronAPIDemosSetup.exe)
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const app = express()
+const url = process.env.URL || 'https://wifi-tracker-server.herokuapp.com'
+const port = Number(process.env.PORT) || 5000
+const downloadUrl = 'https://github.com/zeke/wifi-tracker/releases/download'
+
+app.set('port', port)
+
+app.listen(app.get('port'), () => {
+  console.log('Server is running on port', app.get('port'))
+})
+
+app.get('/', (request, response) => {
+  response.send('Wifi Tracker Updates Server')
+})
+
+app.get('/updates/:asset', (request, response) => {
+  const asset = request.params.asset
+  const version = process.env.LATEST_WIFI_TRACKER_RELEASE
+  response.redirect(`${downloadUrl}/v${version}/${asset}`)
+})
+
+app.get('/updates', (request, response) => {
+  const version = request.query.version
+  const latestRelease = process.env.LATEST_WIFI_TRACKER_RELEASE
+  if (version === latestRelease) {
+    response.status(204).end()
+  } else {
+    response.json({
+      name: `Wifi Tracker v${latestRelease}`,
+      notes: 'The latest version of Wifi Tracker',
+      pub_date: new Date().toISOString(),
+      url: `${url}/updates/wifi-tracker-darwin-x64.zip`
+    })
+  }
+})
